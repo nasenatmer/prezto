c1d9df43326c4812cd96221bbd79730582445056
nasenatmer <nasenatmer@posteo.de>
2015-02-16 12:50:22 +0100

    Comprehensive update of aliases

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1,45 +1,23 @@
-#n paar Erleichterungen fuer den Alltag
+# Most common options
 alias llt="ls -lht"
 alias lls="ls -lhS"
 alias llsr="ls -lhSr"
-alias mkmine="sudo chown -R ${USER}:users"
-
-alias back="cd -"
 alias cl="clear"
 alias ccl="cd;cl"
 
 alias cp="cp -p"
 alias cpp='rsync -avh --stats --progress'
-alias cppn='rsync -avnh --stats --progress'
-alias rm="rm -I"
+alias cppn='rsync -avhn --stats --progress'
 alias mv="nocorrect mv"
-alias ctie="nocorrect setxkbmap de"
-alias asdf="nocorrect setxkbmap de neo"
+alias rm="rm -I"
 
-alias duh1="du -h --max-depth=1 | sort -n"
-
-if [[ -x /usr/bin/discus ]]; then
-alias df="discus"
-elif [[ -s /usr/bin/dfc ]]; then
-alias df="dfc"
-fi
-
-if [[ -s ~/tmp/git/k/k.sh ]]; then
-	source ~/tmp/git/k/k.sh
-fi
-
-if [[ -x /usr/bin/umount.udisks2 ]]; then
-	alias umount="umount.udisks2"
-fi
-
-######################################## mpd
 alias m="mpc"
 alias mt="mpc toggle"
 alias nc="ncmpcpp"
 
-#Progs starten
 alias v="vim --servername vim"
 alias sv="sudo vim -u ~/.vimrc"
+alias duh1="du -h --max-depth=1 | sort -n"
 alias locu="sudo updatedb"
 alias loc="locate"
 alias calc="wcalc"
@@ -47,22 +25,9 @@ alias mp="mplayer -nolirc -nojoystick -noar -fs -osdlevel 3"
 alias fn="find -name"
 alias top="htop"
 alias bvg="xdg-open /home/jakob/doc/diverses/bvg-netz.pdf &> /dev/null &"
-alias -g G='| grep'
-alias -g T='| tail'
-alias -s rar='rar x'
-alias -s odt="background /usr/bin/soffice --writer"
-alias -s doc="background /usr/bin/soffice --writer"
-alias -s docx="background /usr/bin/soffice --writer"
-alias -s ods="background /usr/bin/soffice --calc"
-alias -s xls="background /usr/bin/soffice --calc"
-alias -s odp="background /usr/bin/soffice --impress"
-alias -s ppt="background /usr/bin/soffice --impress"
-alias -s pptx="background /usr/bin/soffice --impress"
-alias -s pdf="background xdg-open"
-alias -s PDF="background xdg-open"
-alias -s html="background xdg-open"
-alias pastetmux="curlpaste -f /tmp/.tmux-exchange"
-alias wcu="wicd-curses"
+alias mkmine="sudo chown -R ${USER}:users"
+alias ctie="nocorrect setxkbmap de"
+alias asdf="nocorrect setxkbmap de neo"
 
 #Pacman shortcuts
 alias p="nocorrect yaourt"
@@ -71,12 +36,27 @@ alias pqs="nocorrect yaourt -Qs"
 alias pqi="nocorrect yaourt -Qi"
 alias pql="nocorrect yaourt -Ql"
 alias pS="nocorrect yaourt -S"
-alias pss="nocorrect yaourt -Ss"
 alias psi="nocorrect yaourt -Si"
 alias psyu="nocorrect yaourt -Syu"
 alias pR="nocorrect yaourt -R"
 alias prss="nocorrect yaourt -Rssu"
 alias pU="nocorrect yaourt -U"
+
+# Global and file aliases
+alias -g G='| grep'
+alias -g T='| tail'
+alias -s odt="background /usr/bin/soffice --writer"
+alias -s doc="background /usr/bin/soffice --writer"
+alias -s docx="background /usr/bin/soffice --writer"
+alias -s ods="background /usr/bin/soffice --calc"
+alias -s xls="background /usr/bin/soffice --calc"
+alias -s xlsx="background /usr/bin/soffice --calc"
+alias -s odp="background /usr/bin/soffice --impress"
+alias -s ppt="background /usr/bin/soffice --impress"
+alias -s pptx="background /usr/bin/soffice --impress"
+alias -s pdf="background xdg-open"
+alias -s PDF="background xdg-open"
+alias -s html="background xdg-open"
 
 # systemd stuff
 alias sc="nocorrect systemctl"
@@ -93,3 +73,22 @@ alias ssc="nocorrect sudo systemctl"
     0.log() {
         nocorrect sudo journalctl
     }
+    
+# A few special aliases depending on installed programmes
+if [[ -x /usr/bin/discus ]]; then
+	alias df="discus"
+elif [[ -s /usr/bin/dfc ]]; then
+	alias df="dfc"
+else
+	alias df="df -h"
+fi
+
+if [[ -s ~/tmp/git/k/k.sh ]]; then
+	source ~/tmp/git/k/k.sh
+fi
+
+if [[ -x /usr/bin/umount.udisks2 ]]; then
+	alias umount="umount.udisks2"
+fi
+
+
